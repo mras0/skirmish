b2188dac2007f316ade89c661c28e0de9828e107
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2016-04-16 21:27:35 +0200

    Reafctor and extend vec

--- test_math.cpp
+++ test_math.cpp
@@ -2,8 +2,9 @@
 #include <type_traits>
 #include <ostream>
 
-template<typename T, typename tag>
-std::ostream& operator<<(std::ostream& os, const skirmish::vec3<T, tag>& v) {
+template<unsigned size, typename T, typename tag>
+std::ostream& operator<<(std::ostream& os, const skirmish::vec<size, T, tag>& v) {
+    static_assert(size == 3, "");
     return os << "{ " << v.x << ", " << v.y << ", " << v.z << " }";
 }
 
@@ -17,13 +18,13 @@ using vec3ft1 = vec3f<tag1>;
 
 static_assert(std::is_pod<vec3ft1>::value, "vec3 must be POD!");
 
-TEST_CASE("Construction works") {
+TEST_CASE("vec3 construction") {
     REQUIRE((vec3ft1{1,2,3}).x == 1);
     REQUIRE((vec3ft1{1,2,3}).y == 2);
     REQUIRE((vec3ft1{1,2,3}).z == 3);
 }
 
-TEST_CASE("Boolean logic") {
+TEST_CASE("vec3 boolean logic") {
     const vec3ft1 a{1,2,3};
     const vec3ft1 b{1.001f,2,3};
     const vec3ft1 c{1,2.1f,3};
@@ -42,18 +43,54 @@ TEST_CASE("Boolean logic") {
     REQUIRE(d != c);
 }
 
-TEST_CASE("Multiplication") {
+TEST_CASE("vec3 multiplication") {
     vec3ft1 a{10, 20, 100};
     SECTION("negative") {
-        a *= -2;
-        REQUIRE(a == (vec3ft1{-20, -40, -200}));
+        REQUIRE(a * -2.0f == (vec3ft1{-20, -40, -200}));
+        REQUIRE(-2.0f * a == (vec3ft1{-20, -40, -200}));
+        REQUIRE((a *= -2) == (vec3ft1{-20, -40, -200}));
     }
     SECTION("zero") {
-        a *= 0;
-        REQUIRE(a == (vec3ft1{0, 0, 0}));
+        REQUIRE((a *= 0) == (vec3ft1{0, 0, 0}));
     }
     SECTION("positive") {
-        a *= 1.1f;
-        REQUIRE(a == (vec3ft1{11, 22, 110}));
+        REQUIRE((a *= 1.1f) == (vec3ft1{11, 22, 110}));
+    }
+}
+
+TEST_CASE("vec3 addition") {
+    vec3ft1 a{1, 2, 3};
+    vec3ft1 b{-3, 2.5f, 10};
+    const vec3ft1 sum{-2, 4.5f, 13};
+    REQUIRE(a + b == sum);
+    REQUIRE(b + a == sum);
+    SECTION("a+=b") {
+        REQUIRE((a += b) == sum);
+    }
+    SECTION("b+=a") {
+        REQUIRE((b += a) == sum);
+    }
+}
+
+TEST_CASE("vec3 negation") {
+    const vec3ft1 a{7, 4, -1};
+    const vec3ft1 na{-7, -4, 1};
+    REQUIRE(-a == na);
+    REQUIRE(-na == a);
+    REQUIRE(-(-a) == a);
+}
+
+TEST_CASE("vec3 subtraction") {
+    vec3ft1 a{1, 2, 3};
+    vec3ft1 b{-3, 2.5f, 10};
+    const vec3ft1 diff_ab{4, -0.5f, -7};
+    const vec3ft1 diff_ba{-4, 0.5f, 7};
+    REQUIRE(a - b == diff_ab);
+    REQUIRE(b - a == diff_ba);
+    SECTION("a-=b") {
+        REQUIRE((a -= b) == diff_ab);
+    }
+    SECTION("b-=a") {
+        REQUIRE((b -= a) == diff_ba);
     }
 }
--- vec.h
+++ vec.h
@@ -3,30 +3,91 @@
 
 namespace skirmish {
 
+template<unsigned Size, typename T, typename tag>
+struct vec;
+
+// vec3
 template<typename T, typename tag>
-struct vec3 {
+struct vec<3, T, tag> {
     T x, y, z;
 
-    vec3& operator*=(T rhs) {
+    vec operator-() const {
+        return { -x, -y, -z };
+    }
+
+    vec& operator+=(const vec& rhs) {
+        x += rhs.x;
+        y += rhs.y;
+        z += rhs.z;
+        return *this;
+    }
+
+    vec& operator-=(const vec& rhs) {
+        x -= rhs.x;
+        y -= rhs.y;
+        z -= rhs.z;
+        return *this;
+    }
+
+    template<typename Y>
+    vec& operator*=(Y rhs) {
         x *= rhs;
         y *= rhs;
         z *= rhs;
         return *this;
     }
+
+    constexpr T operator[](unsigned index) const {
+        return index == 0 ? x : index == 1 ? y : z;
+    }
 };
 
-template<typename T, typename tag>
-bool operator==(const vec3<T, tag>& l, const vec3<T, tag>& r) {
-    return l.x == r.x && l.y == r.y && l.z == r.z;
+// Generic
+template<unsigned Size, typename T, typename tag>
+bool operator==(const vec<Size, T, tag>& l, const vec<Size, T, tag>& r) {
+    for (unsigned i = 0; i < Size; ++i) {
+        if (l[i] != r[i])
+            return false;
+    }
+    return true;
 }
 
-template<typename T, typename tag>
-bool operator!=(const vec3<T, tag>& l, const vec3<T, tag>& r) {
+template<unsigned Size, typename T, typename tag>
+bool operator!=(const vec<Size, T, tag>& l, const vec<Size, T, tag>& r) {
     return !(l == r);
 }
 
+template<unsigned Size, typename T, typename tag, typename Y>
+vec<Size, T, tag> operator*(const vec<Size, T, tag>& l, Y r) {
+    auto res = l;
+    res *= r;
+    return res;
+}
+
+
+template<unsigned Size, typename T, typename tag, typename Y>
+vec<Size, T, tag> operator*(Y l, const vec<Size, T, tag>& r) {
+    auto res = r;
+    res *= l;
+    return res;
+}
+
+template<unsigned Size, typename T, typename tag>
+vec<Size, T, tag> operator+(const vec<Size, T, tag>& l, const vec<Size, T, tag>& r) {
+    auto res = l;
+    res += r;
+    return res;
+}
+
+template<unsigned Size, typename T, typename tag>
+vec<Size, T, tag> operator-(const vec<Size, T, tag>& l, const vec<Size, T, tag>& r) {
+    auto res = l;
+    res -= r;
+    return res;
+}
+
 template<typename tag>
-using vec3f = vec3<float, tag>;
+using vec3f = vec<3, float, tag>;
 
 
 } // namespace skirmish
